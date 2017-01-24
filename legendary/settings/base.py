3a4703e6a457bd3d1bf97e3d4d3487053f81456b
David Hynek <David Hynek>
2017-01-24 00:07:14 -0500

    Adding more formatting and styling for the card display view. Setting the allowed hosts to localhost or the legendary-online.com domain.

--- legendary/settings/base.py
+++ legendary/settings/base.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pf-@jxtojga)z+4s*uwbgjrq$aep62-thd0q7f&o77xtpka!_m'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '.legendary-online.com']
 
 # Application definition
 
 INSTALLED_APPS = (
-	'cards',
+    'cards',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
